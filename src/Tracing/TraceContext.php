dac083190d874de431c6ee0041d1e03df944f91c
Diego Henrique de Souza Ananias - Unicred do Brasil <diego.ananias@e-unicred.com.br>
2026-07-24 15:45:10 -0300

    feat: add TraceContext for coroutine-safe trace propagation

---
+++ src/Tracing/TraceContext.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chronos\Tracing;
+
+use Hyperf\Context\Context;
+
+class TraceContext
+{
+    protected const TRACE_KEY = 'chronos.trace_id';
+    protected const PARENT_KEY = 'chronos.parent_job_id';
+
+    public static function getTraceId(): string
+    {
+        if (class_exists(Context::class) && Context::has(self::TRACE_KEY)) {
+            return (string) Context::get(self::TRACE_KEY);
+        }
+
+        return self::generateTraceId();
+    }
+
+    public static function setTraceId(string $traceId): void
+    {
+        if (class_exists(Context::class)) {
+            Context::set(self::TRACE_KEY, $traceId);
+        }
+    }
+
+    public static function getParentJobId(): ?string
+    {
+        if (class_exists(Context::class) && Context::has(self::PARENT_KEY)) {
+            return (string) Context::get(self::PARENT_KEY);
+        }
+
+        return null;
+    }
+
+    public static function setParentJobId(?string $jobId): void
+    {
+        if (class_exists(Context::class)) {
+            Context::set(self::PARENT_KEY, $jobId);
+        }
+    }
+
+    public static function generateTraceId(): string
+    {
+        $traceId = 'trc_' . bin2hex(random_bytes(8));
+        self::setTraceId($traceId);
+        return $traceId;
+    }
+}
